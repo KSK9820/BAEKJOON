fbc53efc1536fede34f2f4b52d9c7531627eaf93
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2021-02-15 02:57:56 +0900

    Create 9205.cpp
    9205: 맥주 마시면서 걸어가기: BFS

---
+++ 9205.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <queue>
+using namespace std;
+
+int t, n;
+//int startx, starty;
+//int endx, endy;
+vector<pair<int, int>> cs;
+
+bool BFS(int x, int y) {
+	
+	// (x,y) 큐 생성
+	queue<pair<int, int>> q;
+	q.push(make_pair(x, y));
+
+	//방문 여부 체크
+	vector<bool> visited;
+	visited = vector<bool>(n + 2, false);
+	//초기 방문 체크
+	visited[0] = true;
+
+
+	while (!q.empty()) {
+		int point_x = q.front().first;
+		int point_y = q.front().second;
+		q.pop();
+
+		for (int i = 1; i < n + 2; ++i) {
+			int next_x = cs[i].first;
+			int next_y = cs[i].second;
+
+			if (visited[i] == false && ((next_x - point_x) + (next_y - point_y)) <= 1000) {
+				q.push(make_pair(cs[i].first, cs[i].second));
+				visited[i] = true;
+				//true면 다시 볼 필요가 없으니까
+			}
+		}
+	}
+	return visited[n + 1];
+}
+
+int main(void) {
+	cin >> t;
+
+	while (t--) {
+		cin >> n;
+		cs.clear();
+		
+//		cin >> startx >> starty;
+//		cs.push_back(make_pair(startx, starty));
+		
+		for (int i = 0; i < n+2; i++) {
+			int x, y;
+			cin >> x >> y;
+			cs.push_back(make_pair(x, y));
+		}
+
+//		cin >> endx >> endy;
+//		cs.push_back(make_pair(endx, endy));
+
+//		bool result = BFS(startx, starty);
+		bool result = BFS(cs[0].first, cs[0].second);
+		
+		if (result == true) {
+			cout << "happy" << endl;
+		}
+		else {
+			cout << "sad" << endl;
+		}
+	}
+	//return 0;
+}
+
